181e4fe61b0ce08608a6c2d3646b57d92bcd9267
Lucas Maciel <lucas333maciel@gmail.com>
2020-04-21 20:57:00 -0300

    finish desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -5,7 +5,18 @@ interface Balance {
   outcome: number;
   total: number;
 }
+interface Request {
+  title: string;
 
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
+enum Type {
+  INCOME = 'income',
+  OUTCOME = 'outcome',
+}
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +25,35 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
+  }
+
+  private calculateBalance(operator: Type): number {
+    return this.transactions
+      .filter(transaction => transaction.type === operator)
+      .reduce((acc, transaction) => acc + transaction.value, 0);
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.calculateBalance(Type.INCOME);
+    const outcome = this.calculateBalance(Type.OUTCOME);
+    const total = income - outcome;
+
+    const balance: Balance = { income, outcome, total };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Request): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    const { total } = this.getBalance();
+
+    if (type === Type.OUTCOME && total < value) {
+      throw Error('Cannot create outcome transaction without a valid balance');
+    }
+
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
